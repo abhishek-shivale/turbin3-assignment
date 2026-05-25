dc803655efa7493ee62ff60adfd217f25b058907
Abhishek shivale <abhishekshivale45@gmail.com>
2026-05-25 18:32:10 +0530

    feat: started working on amm

---
+++ assignment-5/programs/assignment-5/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("6NvaFi7fwTf4nnK5sMJKRHgEKDyD5FZUTsE5cDb1ZQf2");
+
+#[program]
+pub mod assignment_5 {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
